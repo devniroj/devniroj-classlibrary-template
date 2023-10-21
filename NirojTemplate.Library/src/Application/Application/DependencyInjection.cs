23cb8fe90536995c693775a6796d018716e1fdc1
Niroechkumar Sugumar <snero90@gmail.com>
2023-10-21 21:21:53 +0800

    directory build props added

---
+++ NirojTemplate.Library/src/Application/Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NirojTemplate.Library.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
