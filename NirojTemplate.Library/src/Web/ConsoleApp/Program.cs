698bd04d095b8deeaa417f0746f639bdec470750
Niroechkumar Sugumar <snero90@gmail.com>
2023-11-18 21:10:54 +0800

    console app added

---
+++ NirojTemplate.Library/src/Web/ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+try
+{
+	var configBuilder = new ConfigurationBuilder();
+	configBuilder.AddJsonFile("appsettings.json");
+	var configuration = configBuilder.Build();
+
+	var host = Host.CreateDefaultBuilder(args)
+		.ConfigureServices((context, services) =>
+		{
+
+		}).Build();
+
+	Environment.Exit(0);
+}
+catch (Exception)
+{
+
+	throw;
+}
