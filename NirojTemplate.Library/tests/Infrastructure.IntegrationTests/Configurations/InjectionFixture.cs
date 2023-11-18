a5aef4b4feead66b07bb014052d9462ab8a5b66b
Niroechkumar Sugumar <snero90@gmail.com>
2023-11-18 21:22:01 +0800

    integration testing added

---
+++ NirojTemplate.Library/tests/Infrastructure.IntegrationTests/Configurations/InjectionFixture.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NirojTemplate.Library.Infrastructure.IntegrationTests.Configurations
+{
+    public class InjectionFixture : IDisposable
+    {
+        private readonly IHost _host;
+        public InjectionFixture()
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables().Build();
+
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, service) =>
+                {
+
+                }).Build();
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IServiceProvider ServiceProvider => _host.Services;
+        public IConfigurationRoot Configuration { get; }
+    }
+}
--- NirojTemplate.Library/tests/Infrastructure.IntegrationTests/UnitTest1.cs
+++ NirojTemplate.Library/tests/Infrastructure.IntegrationTests/UnitTest1.cs
@@ -2,6 +2,10 @@ namespace NirojTemplate.Library.Infrastructure.IntegrationTests
 {
     public class UnitTest1:IClassFixture<IntegrationTestWebApplicationFactory>
     {
+        public UnitTest1()
+        {
+
+        }
         //test comment
         [Fact]
         public void Test1()
